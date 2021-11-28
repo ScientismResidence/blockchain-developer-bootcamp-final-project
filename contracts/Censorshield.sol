909563705b9b0700c7fe8afc0b34f151b27bc4ac
dmitry.urusov <urusov@finmarkets.org>
2021-11-28 18:40:16 +0200

    Comments for a contract

--- contracts/Censorshield.sol
+++ contracts/Censorshield.sol
@@ -2,12 +2,22 @@ pragma solidity 0.8.10;
 
 import "./util/Strings.sol";
 
+/// @title Contract for distributing content protected from censorship
+/// @author Dmitry Urusov
+/// @notice content is stored as Proof of Existence concept
 contract Censorshield {
     
+    /// @notice Maximum string length for group names
     uint8 public constant GroupNameMaxLength = 32;
+
+    /// @notice Maximum string length for content names
     uint8 public constant ItemNameMaxLength = 128;
+
+    /// @notice Price to change the storage
+    /// @dev There should be more advanced way to impact on user intention for storage change
     uint public constant Price = 0.1 ether;
     
+    /// @notice Struct that represents group content
     struct Item {
         uint id;
         bytes32 hash;
@@ -18,6 +28,7 @@ contract Censorshield {
         bool isAccepted;
     }
 
+    /// @notice Struct that represents content
     struct Group {
         uint id;
         address creator;
@@ -32,6 +43,7 @@ contract Censorshield {
     }
 
     /// @notice Keep the information about contract owner to handle the security
+    /// @dev Currently sensless. TODO Implement the proxy contract to be able deploy the updates on existed contract (will be very interesting)
     address public owner = msg.sender;
 
     /// @notice Mapping of groups ids to group struct.
@@ -58,14 +70,26 @@ contract Censorshield {
     /// @notice Item counter to handle the item ids
     uint public itemCounter;
 
+    /// @notice Emitted when new group is added
+    /// @param groupId group identificator
+    /// @param name group name
+    /// @param creator creator of the group
     event LogAddGroup(uint indexed groupId, string indexed name, address indexed creator);
+
+    /// @notice Emitted when new content is added to group
+    /// @param groupId group identificator to which new content is added
+    /// @param itemId content identificator
+    /// @param hash hash to check the content existence
+    /// @param author creator of the content
     event LogAddItem(uint indexed itemId, uint indexed groupId, bytes32 hash, address author);
 
+    /// @notice Handle the requirement that transaction has enough fund
     modifier paidEnough() { 
         require(msg.value >= Price, "Not enough funds"); 
         _;
     }
     
+    /// @notice Handle a case when user send extra funds and refund it back
     modifier refundExcess() {
         _;
         uint amountToRefund = msg.value - Price;
@@ -73,6 +97,11 @@ contract Censorshield {
             payable(msg.sender).transfer(amountToRefund);
     }
 
+    /// @notice Adds new group and assign the sender as a member of this group.
+    /// Setup the rules for changing status of content that belongs to this group.
+    /// @param name Unique group name
+    /// @param _minimalVotes Minimal votes required to change status of the group content
+    /// @param _minimalPercentsToAccept Minimal percents of group members to change the group content status (draft to published)
     function addGroup(string memory name, uint32 _minimalVotes, uint8 _minimalPercentsToAccept)
         public
         payable
@@ -119,6 +148,10 @@ contract Censorshield {
         emit LogAddGroup(groupCounter, group.name, msg.sender);
     }
 
+    /// @notice Adds new content to the group
+    /// @param groupId Group identification to which the content will belong
+    /// @param name Content name
+    /// @param _hash Hash of the content to provide Proof of Existence concept for related content that off-chained stored
     function addItem(uint groupId, string memory name, bytes32 _hash)
         public
         payable
@@ -149,6 +182,8 @@ contract Censorshield {
         emit LogAddItem(item.id, item.groupId, _hash, item.author);
     }
 
+    /// @notice Getter to provide an information about group by its identificator
+    /// @param groupId Group identification for desired group
     function getGroup(uint groupId)
         public
         view
@@ -170,11 +205,31 @@ contract Censorshield {
         memberCounter = group.memberCounter;
     }
 
+    /// @notice Getter to provide a content identificator by index of this content
+    /// @param groupId Group identification to which belong the requested content
+    /// @param index Index of the content for that group
     function getGroupContentId(uint groupId, uint index)
         public
         view
         returns (uint id)
     {
         id = groupsMap[groupId].content[index];
+    }
+
+    /// @notice TODO Allows to vote for a content to move it to published status
+    function vote(uint contentId)
+        public
+    {
+
+    }
+
+    /// @notice TODO Allows to invite a new member to group
+    function invite(uint groupId, address newMember)
+        public
+        payable
+        paidEnough
+        refundExcess
+    {
+
     }
 }
